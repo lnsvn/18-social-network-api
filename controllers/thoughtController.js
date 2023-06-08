3d755b6eec7a48b6b58f967580140dfcbc1c0ab9
Lauren Sullivan <122634162+lnsvn@users.noreply.github.com>
2023-06-08 13:40:12 -0400

    creates thought controllers and routes for getting all or one thought

---
+++ controllers/thoughtController.js
@@ -0,0 +1,27 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+  // get all thoughts
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find({});
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // get a single thought
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId }).select('-__v');
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with that ID' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { getThoughts, getSingleThought} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts);
+
+router.route('/:thoughtId').get(getSingleThought);
+
+module.exports = router;
